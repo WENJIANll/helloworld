8b6b523769df0b97cfb6873235ea3620e246d8ad
WUYNE <1305133324@qq.com>
2020-12-22 10:24:56 +0800

    国际化

--- src/main/java/com/myspringboot/helloworld/config/MyMvcConfig.java
+++ src/main/java/com/myspringboot/helloworld/config/MyMvcConfig.java
@@ -1,7 +1,11 @@
 package com.myspringboot.helloworld.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // 扩展spring MVC
@@ -9,5 +13,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registratry){
+        registratry.addViewController("/").setViewName("index");
+        registratry.addViewController("index.html").setViewName("index");
+        registratry.addViewController("main.html").setViewName("dashboard");
+    }
+
+    // 自定义国际划
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
+    }
 
 }
